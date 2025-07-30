c25735c35bfc94b367bc67400f4bb8f366709339
Jorge Beserra <jorgebeserra@gmail.com>
2025-07-30 11:50:03 -0300

    Update can_manager.cpp

--- src/can_manager.cpp
+++ src/can_manager.cpp
@@ -277,6 +277,7 @@ void CANManager::loop()
             {
                 // Aqui o Raspberry deve interpretar esse comando e desligar
                 Logger::console("ShuttingDown");
+                
                 digitalWrite(MOSFET_PIN, LOW);  // Desativa o MOSFET
 
                 Serial.println("Entering sleep mode...");
